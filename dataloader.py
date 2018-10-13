1b865217bfe1e5e26188e0d671f921e0b9a6428a
Yuan-T-Xuan <tonyxuan26@foxmail.com>
2018-10-12 22:36:06 -0400

    add & change to tradesy data

--- dataloader.py
+++ dataloader.py
@@ -12,3 +12,27 @@ def load_citeulike():
     
     return raw_data
 
+def load_lastfm():
+
+    raw_data = dict()
+    raw_data['total_users'] = 992
+    raw_data['total_items'] = 14598
+
+    raw_data['train_data'] = np.load('dataset/lastfm/rsrf_user_data_train.npy')
+    raw_data['val_data'] = np.load('dataset/lastfm/rsrf_user_data_test.npy')
+    raw_data['test_data'] = np.load('dataset/lastfm/rsrf_user_data_test.npy')
+    
+    return raw_data
+
+def load_tradesy():
+
+    raw_data = dict()
+    raw_data['total_users'] = 19243
+    raw_data['total_items'] = 165906
+    
+    raw_data['train_data'] = np.load('dataset/tradesy/rsrf_user_data_train.npy')
+    raw_data['val_data'] = np.load('dataset/tradesy/rsrf_user_data_val.npy')
+    raw_data['test_data'] = np.load('dataset/tradesy/rsrf_user_data_test.npy')
+
+    return raw_data
+
