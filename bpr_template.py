d52299a398e2c6591c1ef3a600bb070bb123b76a
Yuan-T-Xuan <tonyxuan26@foxmail.com>
2018-10-30 20:55:14 -0400

    clean code & work on Tradesy data

--- bpr_template.py
+++ bpr_template.py
@@ -1,12 +1,13 @@
 from openrec import ModelTrainer
 from openrec.utils import Dataset
-from PMLP import PMLP
-from openrec.utils.evaluators import AUC, Recall
-from openrec.utils.samplers import StratifiedPointwiseSampler
+from BPR import BPR
+from openrec.utils.evaluators import AUC
+from openrec.utils.samplers import RandomPairwiseSampler
 from openrec.utils.samplers import EvaluationSampler
 import dataloader
 
-raw_data = dataloader.load_citeulike()
+#raw_data = dataloader.load_citeulike()
+raw_data = dataloader.load_tradesy()
 dim_embed = CHANGE_DIM_HERE
 total_iter = 10000
 batch_size = 1000
@@ -17,18 +18,17 @@ train_dataset = Dataset(raw_data['train_data'], raw_data['total_users'], raw_dat
 val_dataset = Dataset(raw_data['val_data'], raw_data['total_users'], raw_data['total_items'], name='Val', num_negatives=500)
 test_dataset = Dataset(raw_data['test_data'], raw_data['total_users'], raw_data['total_items'], name='Test', num_negatives=500)
 
-train_sampler = StratifiedPointwiseSampler(pos_ratio=0.2, batch_size=batch_size, dataset=train_dataset, num_process=5)
+train_sampler = RandomPairwiseSampler(batch_size=batch_size, dataset=train_dataset, num_process=5)
 val_sampler = EvaluationSampler(batch_size=batch_size, dataset=val_dataset)
 test_sampler = EvaluationSampler(batch_size=batch_size, dataset=test_dataset)
 
-model = PMLP(batch_size=batch_size, total_users=train_dataset.total_users(), total_items=train_dataset.total_items(), 
-             l2_reg=CHANGE_L2_REG_HERE,
-             mlp_dims=CHANGE_MLP_DIMS_HERE, dim_user_embed=dim_embed, dim_item_embed=dim_embed, save_model_dir='pmlp_recommender/', train=True, serve=True)
+bpr_model = BPR(batch_size=batch_size, total_users=train_dataset.total_users(), total_items=train_dataset.total_items(), 
+                l2_reg=CHANGE_L2_REG_HERE,
+                dim_user_embed=dim_embed, dim_item_embed=dim_embed, save_model_dir='bpr_recommender/', train=True, serve=True)
+
+model_trainer = ModelTrainer(model=bpr_model)
 
 auc_evaluator = AUC()
-recall_evaluator = Recall(recall_at=[10, 20, 30, 40, 50, 60, 70, 80, 90, 100])    
-model_trainer = ModelTrainer(model=model)
-
 model_trainer.train(total_iter=total_iter, eval_iter=eval_iter, save_iter=save_iter, train_sampler=train_sampler, 
-                    eval_samplers=[val_sampler], evaluators=[auc_evaluator, recall_evaluator])
+                    eval_samplers=[val_sampler], evaluators=[auc_evaluator])
 
--- meta_decoder.py
+++ meta_decoder.py
@@ -3,8 +3,10 @@ import torch
 import torch.nn as nn
 from torch import optim
 import torch.nn.functional as F
-from main import *
 from torch.distributions.categorical import Categorical
+
+from hyper_params import hyper_params
+from calc_reward_given_descriptor import calc_reward_given_descriptor
 
 # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 device = torch.device("cpu")
@@ -21,42 +23,24 @@ class DecoderRNN(nn.Module):
 
     def forward(self, input, hidden):
         # input should be in shape (1,1,-1)
-        print('input:', input.shape)
-        print('hidden:', hidden.shape)
         output, hidden = self.gru(input.view(1, 1, -1), hidden)
         return self.out(output[0]), hidden
 
     def initHidden(self):
         return torch.zeros(1, 1, self.hidden_size, device=device)
 
-hyper_params = [
-    ("dim_of_latent_factor", [25, 50, 75, 100, 125, 150]),
-    ("l2_reg", [0.1, 0.01, 0.001, 0.0001, 0.00001]),
-    ("type_of_interaction", ["PairwiseEuDist", "PairwiseLog", "PointwiseMLPCE"]),
-    ("eudist_margin", [0.5, 1.0, 1.5, 2.0]),
-    ("mlp_dim1", [-1, 50, 75, 100, 125, 150]),
-    ("mlp_dim2", [-1, 50, 75, 100, 125, 150]),
-    ("mlp_dim3", [-1, 50, 75, 100, 125, 150])
-]
-
 def train(meta_decoder, decoder_optimizer, fclayers_for_hyper_params):
-
     decoder_hidden = meta_decoder.initHidden()
     decoder_optimizer.zero_grad()
-    
     output = torch.zeros([1, 1, meta_decoder.output_size], device=device)
     softmax = nn.Softmax(dim=1)
     softmax_outputs_stored = list()
-    
-    
     loss = 0
     # 
     for i in range(3):
         output, decoder_hidden = meta_decoder(output, decoder_hidden)
-        print(hyper_params[i])
-        print('output:', output.shape)
+        #print(hyper_params[i])
         softmax_outputs_stored.append(softmax(fclayers_for_hyper_params[hyper_params[i][0]](output)))
-        print('softmax_outputs_stored:', softmax_outputs_stored)
         #
     output_interaction = softmax_outputs_stored[-1]
     type_of_interaction = Categorical(output_interaction).sample().tolist()[0]
@@ -67,47 +51,38 @@ def train(meta_decoder, decoder_optimizer, fclayers_for_hyper_params):
             softmax_outputs_stored.append(softmax(fclayers_for_hyper_params[hyper_params[i][0]](output)))
     elif type_of_interaction == 1:
         # PairwiseLog
+        # no hyper-params for this interaction type
         pass
     else:
         # PointwiseMLPCE
         for i in range(4, 7):
             output, decoder_hidden = meta_decoder(output, decoder_hidden)
             softmax_outputs_stored.append(softmax(fclayers_for_hyper_params[hyper_params[i][0]](output)))
-    print(len(softmax_outputs_stored))
-    
     # 
     resulted_str = []
     for outputs in softmax_outputs_stored:
         print("softmax_outputs: ", outputs)
-        # idx = torch.argmax(outputs)
         idx = Categorical(outputs).sample()
-        # print('idx:', idx)
-        # resulted_str.append(idx.item())
         resulted_str.append(idx.tolist()[0])
-    resulted_str[2] = type_of_interaction
+    resulted_str[2] = type_of_interaction   # the type of interaction has already been sampled before
     resulted_idx = resulted_str
     resulted_str = "_".join(map(str, resulted_str))
-    print("resulted_str:")
-    print(resulted_str)
-    
+    print("resulted_str: " + resulted_str)
     # 
     reward = calc_reward_given_descriptor(resulted_str)
     print("reward: " + str(reward))
     expectedReward = 0
     for i in range(len(softmax_outputs_stored)):
-        # print(softmax_outputs_stored[i][0].tolist())
-        # print(resulted_idx[i])
         logprob = torch.log(softmax_outputs_stored[i][0][resulted_idx[i]])
         expectedReward += logprob * reward
     loss = - expectedReward   
     print('loss:', loss)
-    
-    # backpropagate the loss according to the policy
+    # finally, backpropagate the loss according to the policy
     loss.backward()
     decoder_optimizer.step()
 
 def trainIters():
-    num_iters = 100
+    num_iters = 1000
   
     meta_decoder = DecoderRNN(100,100)
     fclayers_for_hyper_params = dict()
