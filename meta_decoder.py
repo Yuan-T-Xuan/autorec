7883947b1f4e8657909889a9eb59759dd4d363d3
Kelly Wang <cw823@cornell.edu>
2018-12-11 08:31:08 +0800

    change rnn to mlp

--- meta_decoder.py
+++ meta_decoder.py
@@ -15,62 +15,52 @@ print(device)
 moving_average_alpha = 0.2
 moving_average = -19013 # a MAGIC NUMBER
 
-class DecoderRNN(nn.Module):
+class DecoderMLP(nn.Module):
     def __init__(self, hidden_size, output_size):
-        super(DecoderRNN, self).__init__()
-        self.hidden_size = hidden_size
-        self.output_size = output_size
+        super(DecoderMLP, self).__init__()
+        self.fc1 = nn.Linear(1, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, hidden_size)
+        self.relu = nn.ReLU()
+        
+        self.dimOutput = nn.Linear(hidden_size, output_size[0])
+        self.l2_reg = nn.Linear(hidden_size, output_size[1])
+        self.interaction = nn.Linear(hidden_size, output_size[2])
+        self.eudist_margin = nn.Linear(hidden_size, output_size[3])
+        self.mlp1 = nn.Linear(hidden_size, output_size[4])
+        self.mlp2 = nn.Linear(hidden_size, output_size[5])
+        self.mlp3 = nn.Linear(hidden_size, output_size[6])
 
-        self.gru = nn.GRU(hidden_size, hidden_size)
-        self.out = nn.Linear(hidden_size, output_size)
 
-    def forward(self, input, hidden):
-        # input should be in shape (1,1,-1)
-        output, hidden = self.gru(input.view(1, 1, -1), hidden)
-        return self.out(output[0]), hidden
+    def forward(self, input):
+        out = self.relu(self.fc1(input))
+        out = self.relu(self.fc2(out))
+        out1 = self.dimOutput(out)
+        out2 = self.l2_reg(out)
+        out3 = self.interaction(out)
+        out4 = self.eudist_margin(out)
+        out5 = self.mlp1(out)
+        out6 = self.mlp2(out)
+        out7 = self.mlp3(out)
+        return [out1, out2, out3, out4, out5, out6, out7]
 
-    def initHidden(self):
-        return torch.zeros(1, 1, self.hidden_size, device=device)
 
-def train(meta_decoder, decoder_optimizer, fclayers_for_hyper_params):
+def train(meta_decoder, decoder_optimizer):
     global moving_average
     global moving_average_alpha
-    decoder_hidden = meta_decoder.initHidden()
     decoder_optimizer.zero_grad()
-    output = torch.zeros([1, 1, meta_decoder.output_size], device=device)
+    input = torch.ones([1, 1], device=device)
     softmax = nn.Softmax(dim=1)
     softmax_outputs_stored = list()
     loss = 0
-    # 
-    for i in range(3):
-        output, decoder_hidden = meta_decoder(output, decoder_hidden)
-        #print(hyper_params[i])
-        softmax_outputs_stored.append(softmax(fclayers_for_hyper_params[hyper_params[i][0]](output)))
-        #
-    output_interaction = softmax_outputs_stored[-1]
-    type_of_interaction = Categorical(output_interaction).sample().tolist()[0]
-    if type_of_interaction == 0:
-        # PairwiseEuDist
-        for i in range(3, 4):
-            output, decoder_hidden = meta_decoder(output, decoder_hidden)
-            softmax_outputs_stored.append(softmax(fclayers_for_hyper_params[hyper_params[i][0]](output)))
-    elif type_of_interaction == 1:
-        # PairwiseLog
-        # no hyper-params for this interaction type
-        pass
-    else:
-        # PointwiseMLPCE
-        for i in range(4, 7):
-            output, decoder_hidden = meta_decoder(output, decoder_hidden)
-            softmax_outputs_stored.append(softmax(fclayers_for_hyper_params[hyper_params[i][0]](output)))
-    # 
+
+    output = meta_decoder(input)
+
     resulted_str = []
-    for outputs in softmax_outputs_stored:
-        print("softmax_outputs: ", outputs)
-        idx = Categorical(outputs).sample()
+    for each in output:
+        print("outputs: ", each)
+        idx = Categorical(softmax(each)).sample()
         resulted_str.append(idx.tolist()[0])
-    resulted_str[2] = type_of_interaction   # the type of interaction has already been sampled before
-    resulted_idx = resulted_str
+
     resulted_str = "_".join(map(str, resulted_str))
     print("resulted_str: " + resulted_str)
     # 
@@ -91,6 +81,7 @@ def train(meta_decoder, decoder_optimizer, fclayers_for_hyper_params):
         expectedReward += logprob * reward
     loss = - expectedReward   
     print('loss:', loss)
+
     # finally, backpropagate the loss according to the policy
     loss.backward()
     decoder_optimizer.step()
@@ -98,16 +89,11 @@ def train(meta_decoder, decoder_optimizer, fclayers_for_hyper_params):
 def trainIters():
     num_iters = 1000
   
-    meta_decoder = DecoderRNN(100,100)
-    fclayers_for_hyper_params = dict()
-    
-    for hp in hyper_params:
-        fclayers_for_hyper_params[hp[0]] = nn.Linear(meta_decoder.output_size, len(hp[1]))
-    # print(fclayers_for_hyper_params)
+    meta_decoder = DecoderMLP(100,[6,5,3,4,6,6,6])
 
-    optimizer = optim.Adam(meta_decoder.parameters(), lr=0.001)
+    optimizer = optim.SGD(meta_decoder.parameters(), lr=0.001, momentum=0.9)
     for iteration in range(num_iters):
-        train(meta_decoder, optimizer, fclayers_for_hyper_params)
+        train(meta_decoder, optimizer)
 
 
 if __name__ == "__main__":
